399de12c012193bf05bb16b85ae0026032f41bba
112202407277Christian <112202407277@mhs.dinus.ac.id>
2026-04-18 16:57:11 +0700

    Improve database structure

--- inventaris_db.sql
+++ inventaris_db.sql
@@ -1,16 +1,19 @@
-CREATE DATABASE IF NOT EXISTS inventaris_db;
-USE inventaris_db;
+CREATE DATABASE IF NOT EXISTS db_inventaris;
+USE db_inventaris;
 
-CREATE TABLE IF NOT EXISTS barang (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nama_barang VARCHAR(100) NOT NULL,
-    jumlah INT NOT NULL,
-    harga DECIMAL(10, 2) NOT NULL,
-    tanggal_masuk DATE NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE barang (
+    id_barang INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    nama_barang VARCHAR(100) NOT NULL,
+    jumlah INT NOT NULL,
+    harga DECIMAL(10,2) NOT NULL,
+    tanggal_masuk DATE NOT NULL,
+    gambar VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
